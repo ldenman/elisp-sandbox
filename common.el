5a5c9be1f7941bcdde3593dcc4226e5ae1d0b385
Lake Denman <lake@lakedenman.com>
2017-05-13 12:28:23 -0700

    Add package demo

--- common.el
+++ common.el
@@ -2,12 +2,34 @@
 
 (setq package-enable-at-startup nil)
 
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
+(setq package-archives
+        '(("gnu" . "https://elpa.gnu.org/packages/")
+          ("melpa" . "https://melpa.org/packages/")
+          ("elpy" . "https://jorgenschaefer.github.io/packages/")
+          ("org" . "http://orgmode.org/elpa/")))
 
 (package-initialize)
+
+;;(package-refresh-contents)
 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(unless (package-installed-p 'dash)
+  (package-refresh-contents)
+  (package-install 'dash))
+(require 'dash)
+
+;; load path for other packages
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+(add-to-list 'load-path "~/.emacs.d/elpa/")
+
+;; Moved the custom.el stuff into its own file called ~/.emacs.d/customize.el
+(setq custom-file "~/.emacs.d/customize.el")
+(load custom-file t)
--- run-ci.sh
+++ run-ci.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-HOME=`mktemp -d` emacs --batch --eval "(load-file \"common.el\")(load-file \"$1\")(save-buffers-kill-terminal))"
+HOME=`mktemp -d` emacs -nw  --eval "(progn (load-file \"common.el\")(load-file \"$1\")(load-file \"package-demo/package-demo.el\")(load-file \"$1.demo\")(kill-emacs 0))"

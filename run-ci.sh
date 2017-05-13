658fbf24a6879220093db7dca257bc809110ae55
Lake Denman <lake@lakedenman.com>
2017-05-13 16:18:02 -0700

    create images dir

--- run-ci.sh
+++ run-ci.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm images/*
+mkdir images/
 FOO=$1 HOME=`mktemp -d` ttyrec -e 'emacs --debug-init -nw --eval "(load-file \"loader.el\"))"' "images/$1.ttyrecord"
 seq2gif -w 100 -h 60 -i images/$1.ttyrecord -o images/$1.gif
 echo "done"

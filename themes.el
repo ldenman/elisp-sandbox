6b93d2bd2db5537db85a2084b5944ca441eecf42
Lake Denman <lake@lakedenman.com>
2017-05-14 00:22:27 -0700

    more updates

---
+++ themes.el
@@ -0,0 +1,215 @@
+
+(setq potential-themes-list1 '(basic-theme
+;;                    birds-of-paradise-plus-theme
+;;                    blackboard-theme
+;;                    bliss-theme
+;;                    borland-blue-theme
+;;                    boron-theme
+;;                    bubbleberry-theme
+;;                    busybee-theme
+;;                    calmer-forest-theme
+;;                    caroline-theme
+;;                    challenger-deep-theme               
+;;                    cherry-blossom-theme
+;;                    clues-theme                         
+;;                    ;; colonoscopy-theme
+;;                    ;; creamsody-theme                     
+;;                    ;; cyberpunk-theme
+;;                    ;; dakrone-light-theme
+;;                    ;; dakrone-theme                       
+;;                    ;; danneskjold-theme
+;;                    ;; darcula-theme                       
+;;                    ;; dark-krystal-theme
+;;                    ;; dark-mint-theme                     
+;;                    ;; darkane-theme
+;;                    ;; darkburn-theme                      
+;;                    ;; darkmine-theme
+;;                    ;; darkokai-theme                      
+;;                    ;; darktooth-theme
+;;                    ;; display-theme                       
+;;                    ;; distinguished-theme
+;;                    ;; django-theme                        
+;;                    ;; doom-themes
+;;                    ;; dracula-theme                       
+;;                    ;; eclipse-theme
+;;                    ;; eink-theme                          
+;;                    ;; espresso-theme
+;;                    ;; eziam-theme                         
+;;                    ;; faff-theme
+;;                    ;; farmhouse-theme                     
+;;                    ;; firebelly-theme
+;;                    ;; firecode-theme                      
+;;                    ;; flatland-black-theme
+;;                    ;; flatland-theme                      
+;;                    ;; flatui-dark-theme
+;;                    ;; flatui-theme                        
+;;                    ;; foggy-night-theme
+;;                    ;; forest-blue-theme                   
+;;                    ;; gandalf-theme
+;;                    ;; github-theme                        
+;;                    ;; goose-theme
+;;                    ;; gotham-theme                        
+;;                    ;; grandshell-theme
+;;                    ;; graphene-meta-theme                 
+;;                    ;; green-phosphor-theme
+;;                    ;; green-screen-theme                  
+;;                    ;; greymatters-theme
+;;                    ;; gruber-darker-theme                 
+;;                    ;; gruvbox-theme
+;;                    ;; hamburg-theme                       
+;;                    ;; hc-zenburn-theme
+;;                    ;; helm-themes                         
+;;                    ;; hemisu-theme
+;;                    ;; heroku-theme                        
+;;                    ;; hydandata-light-theme
+;;                    ;; idea-darkula-theme                  
+;;                    ;; inkpot-theme
+;;                    ;; intellij-theme                      
+;;                    ;; iodine-theme
+;;                    ;; ir-black-theme                      
+;;                    ;; jazz-theme
+;;                    ;; jbeans-theme                        
+;;                    ;; kaolin-theme
+;;                    ;; kooten-theme                        
+;;                    ;; kosmos-theme
+;;                    ;; labburn-theme                       
+;;                    ;; lavender-theme
+;;                    ;; lenlen-theme                        
+;;                    ;; leuven-theme
+;;                    ;; light-soap-theme                    
+;;                    ;; liso-theme
+;;                    ;; load-theme-buffer-local             
+;;                    ;; lush-theme
+;;                    ;; madhat2r-theme                      
+;;                    ;; majapahit-theme
+;;                    ;; mandm-theme                         
+;;                    ;; material-theme
+;;                    ;; mbo70s-theme                        
+;;                    ;; melancholy-theme
+;;                    ;; mellow-theme                        
+;;                    ;; metalheart-theme
+;;                    ;; minimal-theme                       
+;;                    ;; moe-theme
+;;                    ;; molokai-theme                       
+;;                    ;; monochrome-theme
+;;                    ;; monokai-alt-theme                   
+;;                    ;; monokai-theme
+;;                    ;; mustang-theme                       
+;;                    ;; mustard-theme
+;;                    ;; naquadah-theme                      
+;;                    ;; niflheim-theme
+;;                    ;; nimbus-theme                        
+;;                    ;; noctilux-theme
+;;                    ;; nord-theme                          
+;;                    ;; obsidian-theme
+;;                    ;; occidental-theme                    
+;;                    ;; oceanic-theme
+;;                    ;; oldlace-theme                       
+;;                    ;; omtose-phellack-theme
+;;                    ;; org-beautify-theme                  
+;;                    ;; organic-green-theme
+;;                    ;; paganini-theme                      
+;;                    ;; paper-theme
+;;                    ;; pastelmac-theme                     
+;;                    ;; pastels-on-dark-theme
+;;                    ;; peacock-theme                       
+;;                    ;; per-buffer-theme
+;;                    ;; phoenix-dark-mono-theme             
+;;                    ;; phoenix-dark-pink-theme
+;;                    ;; plain-theme                         
+;;                    ;; plan9-theme
+;;                    ;; planet-theme                        
+;;                    ;; prassee-theme
+;;                    ;; professional-theme                  
+;;                    ;; punpun-theme
+;;                    ;; purple-haze-theme                   
+;;                    ;; quasi-monochrome-theme
+;;                    ;; railscasts-reloaded-theme           
+;;                    ;; railscasts-theme
+;;                    ;; rand-theme                          
+;;                    ;; rebecca-theme
+;;                    ;; remember-last-theme                 
+;;                    ;; reverse-theme
+;;                    ;; reykjavik-theme                     
+;;                    ;; select-themes
+;;                    ;; seoul256-theme                      
+;;                    ;; seti-theme
+;;                    ;; sexy-monochrome-theme               
+;;                    ;; silkworm-theme
+;;                    ;; slime-theme                         
+;;                    ;; smart-mode-line-powerline-theme
+;;                    ;; smyx-theme                          
+;;                    ;; soft-charcoal-theme
+;;                    ;; soft-morning-theme                  
+;;                    ;; soft-stone-theme
+;;                    ;; soothe-theme                        
+;;                    ;; sourcerer-theme
+;;                    ;; spacegray-theme                     
+;;                    ;; spacemacs-theme
+;;                    ;; spike-theme                         
+;;                    ;; subatomic-theme
+;;                    ;; subatomic256-theme                  
+;;                    ;; sublime-themes
+;;                    ;; sunny-day-theme                     
+;;                    ;; suscolors-theme
+;;                    ;; svg-mode-line-themes                
+;;                    ;; tango-2-theme
+;;                    ;; tango-plus-theme                    
+;;                    ;; tangotango-theme
+;;                    ;; tao-theme                           
+;;                    ;; termbright-theme
+;;                    ;; tommyh-theme                        
+;;                    ;; toxi-theme
+;;                    ;; tramp-theme                         
+;;                    ;; tronesque-theme
+;;                    ;; twilight-anti-bright-theme          
+;;                    ;; twilight-bright-theme
+;;                    ;; twilight-theme                      
+;;                    ;; ubuntu-theme
+;;                    ;; ujelly-theme                        
+;;                    ;; underwater-theme
+;;                    ;; waher-theme                         
+;;                    ;; warm-night-theme
+;;                    ;; white-sand-theme                    
+;;                    ;; white-theme
+;;                    ;; yoshi-theme
+;;                    ;; zen-and-art-theme                   
+zenburn-theme
+;;                    ;; zerodark-theme                      
+                    ))
+(dolist (theme potential-themes-list1)
+        (package-install theme))
+
+;; until then use the builtins
+
+
+(setq f default-directory)
+(dolist (elt (custom-available-themes))
+  (message (format "%s theme" elt))
+  (load-theme elt t)
+  (find-file (concat f  "examples/ruby.rb"))
+  (split-window-vertically)
+  (dired "/")
+  (split-window-horizontally)
+  (other-window 1)
+  (find-file (concat f "examples/javascript.js"))
+  (other-window -2)
+  (split-window-right)
+  (find-file (concat f  "examples/elisp.el"))
+  ;; (lisp-mode)
+  ;; (other-window 2)
+  ;; (scroll-up-command)
+  ;; (sit-for 1.777)
+  ;; (other-window -3)
+  ;; (scroll-up-command)
+  (sit-for 1.777)
+  (scroll-up-command)
+  (other-window 1)
+  (scroll-up-command)
+  (other-window 2)
+  (scroll-up-command)
+  (sit-for 1.777)
+  (delete-other-windows)
+  )
+
+

6aaf0177e09d800e792e88d3110dff2322e02bc8
Lake Denman <lake@lakedenman.com>
2017-05-14 02:05:45 -0700

    Add term

--- run-ci.sh
+++ run-ci.sh
@@ -2,6 +2,6 @@
 NAME_1=$(echo $1 |cut -f 1 -d '.')
 NAME=`echo $NAME_1-$EMACS_VERSION | tr . -`
 mkdir images/
-TRAVIS_BUILD_DIR=$TRAVIS_BUILD_DIR ELFILE=$1 HOME=$TRAVIS_BUILD_DIR ttyrec -e 'emacs --debug-init -nw --eval "(load-file \"loader.el\"))"' "images/$NAME.ttyrecord"
+TERM=xterm-256color TRAVIS_BUILD_DIR=$TRAVIS_BUILD_DIR ELFILE=$1 HOME=$TRAVIS_BUILD_DIR ttyrec -e 'emacs --debug-init -nw --eval "(load-file \"loader.el\"))"' "images/$NAME.ttyrecord"
 ./bin/seq2gif -w 80 -h 24 -i images/$NAME.ttyrecord -o images/$NAME.gif
 echo "done"

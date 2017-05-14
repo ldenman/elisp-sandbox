4eea78f22799d792ae8a3781059a4850ea276f50
Lake Denman <lake@lakedenman.com>
2017-05-14 01:04:47 -0700

    update ci

--- run-ci.sh
+++ run-ci.sh
@@ -2,6 +2,6 @@
 NAME_1=$(echo $1 |cut -f 1 -d '.')
 NAME=`echo $NAME_1-$EMACS_VERSION | tr . -`
 mkdir images/
-ELFILE=$1 HOME=`mktemp -d` ttyrec -e 'emacs --debug-init -nw --eval "(load-file \"loader.el\"))"' "images/$NAME.ttyrecord"
+ELFILE=$1 HOME=$TRAVIS_BUILD_DIR ttyrec -e 'emacs --debug-init -nw --eval "(load-file \"loader.el\"))"' "images/$NAME.ttyrecord"
 ./bin/seq2gif -w 80 -h 24 -i images/$NAME.ttyrecord -o images/$NAME.gif
 echo "done"

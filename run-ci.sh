3826b0bca1935ad6d5e2e70c44c1a5c8245d7e1b
Lake Denman <lake@lakedenman.com>
2017-05-14 02:52:45 -0700

    Revert "try to change size"
    This reverts commit 093439c87443b830dbff1fe0b300182277ec52d6.

--- run-ci.sh
+++ run-ci.sh
@@ -3,5 +3,5 @@ NAME_1=$(echo $1 |cut -f 1 -d '.')
 NAME=`echo $NAME_1-$EMACS_VERSION | tr . -`
 mkdir images/
 TERM=xterm-256color TRAVIS_BUILD_DIR=$TRAVIS_BUILD_DIR ELFILE=$1 HOME=$TRAVIS_BUILD_DIR ttyrec -e 'emacs --debug-init -nw --eval "(load-file \"loader.el\"))"' "images/$NAME.ttyrecord"
-./bin/seq2gif -w 120 -h 40 -i images/$NAME.ttyrecord -o images/$NAME.gif
+./bin/seq2gif -w 80 -h 24 -i images/$NAME.ttyrecord -o images/$NAME.gif
 echo "done"

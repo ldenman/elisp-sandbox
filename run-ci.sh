67c366d803604545bb4ca195e5a371edba8ab49b
Lake Denman <lake@lakedenman.com>
2017-05-13 19:51:34 -0700

    Try new file name

--- run-ci.sh
+++ run-ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm images/$1.ttyrecord
+NAME=`echo $1-$EMACS_VERSION | tr . -`
 mkdir images/
-FOO=$1 HOME=`mktemp -d` ttyrec -e 'emacs --debug-init -nw --eval "(load-file \"loader.el\"))"' "images/$1.ttyrecord"
-./bin/seq2gif -w 80 -h 24 -i images/$1.ttyrecord -o images/$1.gif
+HOME=`mktemp -d` ttyrec -e 'emacs --debug-init -nw --eval "(load-file \"loader.el\"))"' "images/$1.ttyrecord"
+./bin/seq2gif -w 80 -h 24 -i images/$1.ttyrecord -o images/$NAME.gif
 echo "done"

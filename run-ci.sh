c3165528eaac79e3fe59548babbda24960b4e068
Lake Denman <lake@lakedenman.com>
2017-05-13 17:06:21 -0700

    remove file

--- run-ci.sh
+++ run-ci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm images/*
+rm images/$1.ttyrecord
 mkdir images/
 FOO=$1 HOME=`mktemp -d` ttyrec -e 'emacs --debug-init -nw --eval "(load-file \"loader.el\"))"' "images/$1.ttyrecord"
 ./bin/seq2gif -w 100 -h 60 -i images/$1.ttyrecord -o images/$1.gif

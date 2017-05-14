0c1ec2bae0c96486987b973263934f28a68623e8
Lake Denman <lake@lakedenman.com>
2017-05-14 05:20:08 -0700

    anoja

--- run-ci.sh
+++ run-ci.sh
@@ -4,7 +4,6 @@ NAME_1=$(echo $1 |cut -f 1 -d '.')
 NAME=`echo $NAME_1-$EMACS_VERSION | tr . -`
 if [[ -n $TRAVIS_BUILD_DIR ]]; then
 ls -R
-mkdir images/
 TERM=xterm-256color TRAVIS_BUILD_DIR=$TRAVIS_BUILD_DIR ELFILE=$1 HOME=$TRAVIS_BUILD_DIR ttyrec -e 'emacs --debug-init -nw --eval "(load-file \"loader.el\"))"' "images/$NAME.ttyrecord"
 ./bin/seq2gif -w 80 -h 24 -i images/$NAME.ttyrecord -o images/$NAME.gif
 else

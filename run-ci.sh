1d6eec8ea401a532e13bc6d336d46e0af1b9bcb6
Lake Denman <lake@lakedenman.com>
2017-05-13 20:00:34 -0700

    Again

--- run-ci.sh
+++ run-ci.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-NAME=`echo $1-$EMACS_VERSION | tr . -`
+NAME_1=$(echo $1 |cut -f 1 -d '.')
+NAME=`echo $NAME_1-$EMACS_VERSION | tr . -`
 mkdir images/
-HOME=`mktemp -d` ttyrec -e 'emacs --debug-init -nw --eval "(load-file \"loader.el\"))"' "images/$1.ttyrecord"
-./bin/seq2gif -w 80 -h 24 -i images/$1.ttyrecord -o images/$NAME.gif
+HOME=`mktemp -d` ttyrec -e 'emacs --debug-init -nw --eval "(load-file \"loader.el\"))"' "images/$NAME.ttyrecord"
+./bin/seq2gif -w 80 -h 24 -i images/$NAME.ttyrecord -o images/$NAME.gif
 echo "done"

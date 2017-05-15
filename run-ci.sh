49e689457212a1b1dd806347af56eb3204097ee8
Lake Denman <lake@lakedenman.com>
2017-05-14 19:59:00 -0700

    ok

--- run-ci.sh
+++ run-ci.sh
@@ -2,15 +2,12 @@
 
 NAME_1=$(echo $1 |cut -f 1 -d '.')
 NAME=`echo $NAME_1-$EMACS_VERSION | tr . -`
-
 if [[ -n $TRAVIS_BUILD_DIR ]]; then
 mkdir -p $TRAVIS_BUILD_DIR/images
 HOME=$TRAVIS_BUILD_DIR emacs --batch --eval "(load-file \"preloader.el\")"
-TERM=xterm-256color TRAVIS_BUILD_DIR=$TRAVIS_BUILD_DIR ELFILE=$1 HOME=$TRAVIS_BUILD_DIR ttyrec -e 'emacs --debug-init --batch --eval "(load-file \"loader.el\"))"' "$TRAVIS_BUILD_DIR/images/$NAME.ttyrecord"
+TERM=xterm-256color TRAVIS_BUILD_DIR=$TRAVIS_BUILD_DIR ELFILE=$1 HOME=$TRAVIS_BUILD_DIR ttyrec -e 'emacs --debug-init -nw --eval "(load-file \"loader.el\"))"' "$TRAVIS_BUILD_DIR/images/$NAME.ttyrecord"
 ./bin/seq2gif -w 80 -h 24 -i $TRAVIS_BUILD_DIR/images/$NAME.ttyrecord -o $TRAVIS_BUILD_DIR/images/$NAME.gif
-
 else
-
 emacs_home=`mktemp -d`
 HOME=$emacs_home emacs --batch --eval "(load-file \"preloader.el\")"
 TERM=xterm-256color ELFILE=$1 HOME=$emacs_home ttyrec -e 'emacs --debug-init -nw --eval "(load-file \"loader.el\"))"' "images/$NAME.ttyrecord"

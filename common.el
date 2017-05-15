6e996dda9e0b3441e62ca1c6e5613e1594c8b3ae
Lake Denman <lake@lakedenman.com>
2017-05-14 21:46:00 -0700

    just a test

--- common.el
+++ common.el
@@ -8,34 +8,6 @@
           ("elpy" . "https://jorgenschaefer.github.io/packages/")
           ("org" . "http://orgmode.org/elpa/")))
 
-  (package-initialize)
-
-  (package-refresh-contents)
-
-  ;;Bootstrap `use-package'
-  (unless (package-installed-p 'use-package)
-    (package-refresh-contents)
-    (package-install 'use-package))
-
-(require 'use-package)
-
-
-  (unless (package-installed-p 'dash)
-    (package-refresh-contents)
-    (package-install 'dash))
-
-  (unless (package-installed-p 'seq)
-    (package-refresh-contents)
-    (package-install 'seq))
-
-
-  (setq package-archives
-        '(("gnu" . "https://elpa.gnu.org/packages/")
-          ("melpa" . "https://melpa.org/packages/")
-          ("elpy" . "https://jorgenschaefer.github.io/packages/")
-          ("org" . "http://orgmode.org/elpa/")))
-
-(package-initialize)
 ;; load path for other packages
 
 (add-to-list 'load-path "~/.emacs.d/lisp/")

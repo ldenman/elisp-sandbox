b1a12bf32320b4c2d29a8fab9ed4b1af0790c5cf
Lake Denman <lake@lakedenman.com>
2017-05-15 18:36:03 -0700

    try again

--- .travis/push.sh
+++ .travis/push.sh
@@ -5,14 +5,10 @@ setup_git() {
   git config --global user.name "Travis CI"
 }
 
-commit_website_files() {
-  mkdir images-bak
-  mv images/* images-bak/
-}
-
 upload_files() {
 
-#  git fetch origin-pages
+  git remote add origin-pages https://${GH_TOKEN}@github.com/ldenman/elisp-sandbox.git > /dev/null 2>&1
+  git fetch origin-pages
   git add images/
   git stash
   git checkout gh-pages
@@ -20,14 +16,11 @@ upload_files() {
   emacs index.org --batch -f org-html-export-to-html --kill
   git add index.html images/*.gif
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-  git remote add origin-pages https://${GH_TOKEN}@github.com/ldenman/elisp-sandbox.git > /dev/null 2>&1
   git push --set-upstream origin-pages gh-pages
 }
 
 setup_git
 echo "setup git"
-commit_website_files
-echo "commit web files"
 upload_files
 echo "git push"
 echo "done"

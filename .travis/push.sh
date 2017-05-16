830d3ae2f671238e581789a65301548322a32de9
Lake Denman <lake@lakedenman.com>
2017-05-16 00:33:09 -0700

    c

--- .travis/push.sh
+++ .travis/push.sh
@@ -6,16 +6,20 @@ setup_git() {
 }
 
 upload_files() {
+  echo $(pwd)
   git remote add origin-pages https://${GH_TOKEN}@github.com/ldenman/elisp-sandbox.git > /dev/null 2>&1
   git fetch origin-pages
-  mkdir images2
+  mkdir -p images2
   git status
   for f in `git diff --name-only |grep -e *.gif`; do
+    echo $f
     cp $f images2/
   done
+  git status
   git checkout gh-pages
   git pull origin-pages gh-pages
   mv images2/*.gif images/
+  git status
   git rm images/
   git add images/*.gif
   emacs index.org --batch -f org-html-export-to-html --kill

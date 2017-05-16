75d068398627ad1de0550ddecf3410795fc0eba7
Lake Denman <lake@lakedenman.com>
2017-05-16 01:25:00 -0700

    oh gosh

--- .travis/push.sh
+++ .travis/push.sh
@@ -9,20 +9,21 @@ upload_files() {
   echo $(pwd)
   git remote add origin-pages https://${GH_TOKEN}@github.com/ldenman/elisp-sandbox.git > /dev/null 2>&1
   git fetch origin-pages
-  mkdir -p images2
+#  
+#  mkdir -p images2
+#  git status
+#  var=`$EMACS_VERSION`|tr . -
+#  find images -name "*$var*"|xargs -I {} mv {} images2/
+#  git status
+#  git checkout gh-pages
+#  git pull origin-pages gh-pages
+#  mv images2/*.gif images/
+#  git status
   git status
-  var=`$EMACS_VERSION`|tr . -
-  find images -name "*$var*"|xargs -I {} mv {} images2/
-  git status
-  git checkout gh-pages
-  git pull origin-pages gh-pages
-  mv images2/*.gif images/
-  git status
-  git add images/*.gif
-  emacs index.org --batch -f org-html-export-to-html --kill
-  git add index.html images/*.gif
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-  git push --set-upstream origin-pages gh-pages
+#  emacs index.org --batch -f org-html-export-to-html --kill
+#  git add index.html images/*.gif
+#  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+#  git push --set-upstream origin-pages gh-pages
 }
 
 setup_git

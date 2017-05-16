8a33776dedf6ec267c69facca728b34bebcfe7a2
Lake Denman <lake@lakedenman.com>
2017-05-16 00:22:24 -0700

    wth

--- .travis/push.sh
+++ .travis/push.sh
@@ -8,15 +8,16 @@ setup_git() {
 upload_files() {
   git remote add origin-pages https://${GH_TOKEN}@github.com/ldenman/elisp-sandbox.git > /dev/null 2>&1
   git fetch origin-pages
-  mkdir images-tmp
+  mkdir images2
   git status
   for f in `git diff --name-only |grep -e *.gif`; do
-    cp $f images-tmp/
+    cp $f images2/
   done
-  rm -rf images/*
   git checkout gh-pages
   git pull origin-pages gh-pages
-  mv images-tmp/*.gif images/
+  mv images2/*.gif images/
+  git rm images/
+  git add images/*.gif
   emacs index.org --batch -f org-html-export-to-html --kill
   git add index.html images/*.gif
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"

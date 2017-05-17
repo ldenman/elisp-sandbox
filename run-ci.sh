bd38ef225fcc880de0644fb884e596d6f22b1042
Lake Denman <lake@lakedenman.com>
2017-05-16 17:09:28 -0700

    update test

--- run-ci.sh
+++ run-ci.sh
@@ -9,7 +9,7 @@ HOME=$TRAVIS_BUILD_DIR emacs --batch --eval "(load-file \"preloader.el\")"
 
 
 #TERM=xterm-256color TRAVIS_BUILD_DIR=$TRAVIS_BUILD_DIR ELFILE=$1 HOME=$TRAVIS_BUILD_DIR ttyrec -e 'emacs --debug-init -nw --eval "(load-file \"loader.el\"))"' "$TRAVIS_BUILD_DIR/ttyrecords/$NAME.ttyrecord"
-TERM=xterm-256color ttyrec -e '/bin/bash -c "ls; exit;"' "$TRAVIS_BUILD_DIR/ttyrecords/$NAME.ttyrecord"
+TERM=xterm-256color ttyrec -e 'ls' "$TRAVIS_BUILD_DIR/ttyrecords/$NAME.ttyrecord"
 
 RETVAL=$?
 [ $RETVAL -eq 0 ] && echo TTYREC Reported a successful run && ls -liah $TRAVIS_BUILD_DIR/ttyrecords/
